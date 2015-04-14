78aa72bc6ece20905d86877532f40fddadf224ea
xyit <xiyingit@gmail.com>
2015-04-14 10:45:25 +0800

    ...

--- sim/cache/redis/RedisCache.cpp
+++ sim/cache/redis/RedisCache.cpp
@@ -1,10 +1,11 @@
-#include <RedisCache/RedisCache.h>
+#include <sim/cache/redis/RedisCache.h>
+#include <sim/command.h>
 using namespace std;
 using namespace sim;
 std::string RedisCache::Get(const std::string& key)
 {
     string value;
-    SimRedisReply r = redis_->TryCommand("GET %s", key);
+    SimRedisReply r = redis_->TryCommand("GET %s", key.c_str());
     if (r && r->str) value.append(r->str, r->len);
     return value;
 }
@@ -22,7 +23,7 @@ void RedisCache::Set(const std::string& key,
 void RedisCache::Remove(const std::string& key)
 {
     string value;
-    SimRedisReply r = redis_->TryCommand("DEL %s", key);
+    SimRedisReply r = redis_->TryCommand("DEL %s", key.c_str());
     r.Free();
 }
 
